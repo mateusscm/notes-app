f6c5c92bbc5701d9a925e9e08d96266e7146d75f
mateus.m <mateus.m@juristecplus.com.br>
2021-02-14 18:42:14 -0300

    modal provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../styles/global';
-import { light } from '../styles/themes/light';
-import Layout from '../src/components/Layout';
-
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/global";
+import { light } from "../styles/themes/light";
+import Layout from "../src/components/Layout";
+import { ModalProvider } from "styled-react-modal";
 
 function MyApp({ Component, pageProps }) {
   const theme = light;
@@ -12,16 +12,21 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Notes app</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;800&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <ModalProvider>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ModalProvider>
       </ThemeProvider>
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
