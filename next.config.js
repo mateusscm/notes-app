423ff9f64d0beba29177f97d768daf040f36a2e1
mateus.m <mateus.m@juristecplus.com.br>
2021-02-14 02:09:35 -0300

    stopped tracking next config file

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    env: {
-        MONGO_URI: `mongodb+srv://mirandev:${process.env.REACT_APP_SECRET_PASS}@cluster0.t1e5o.mongodb.net/<dbname>?retryWrites=true&w=majority`
-    }
-}
